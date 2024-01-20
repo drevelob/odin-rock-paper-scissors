61f81443d81d1bff4b8f1a1719deb33cd5139184
drevelob <140353802+drevelob@users.noreply.github.com>
2024-01-20 15:20:09 -0500

    Write function getComputerChoice

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
+
+function getComputerChoice() {
+
+  function getRandomInt(max) {
+    return Math.ceil(Math.random() * max);
+  }
+
+  let computerChoice;
+
+  switch (getRandomInt(3)) {
+    case 1:
+      computerChoice = ROCK;
+      break;
+    case 2:
+      computerChoice = PAPER;
+      break;
+    case 3:
+      computerChoice = SCISSORS;
+      break;
+  }
+
+  return computerChoice;
+}
